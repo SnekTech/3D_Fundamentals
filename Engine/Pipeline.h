648f139234c329d6e56b9e89373fb126b9de60b8
Chili <Chili@users.noreply.github.com>
2018-08-09 08:30:40 +0900

    shared zbuffer and occlusion of light indicator in point scene

--- Engine/Pipeline.h
+++ Engine/Pipeline.h
@@ -8,9 +8,10 @@
 #include "Mat3.h"
 #include "ZBuffer.h"
 #include <algorithm>
+#include <memory>
 
-// triangle drawing pipeline with programmable
-// pixel shading stages
+// triangle drawing pipeline with programable
+// pixel shading stage
 template<class Effect>
 class Pipeline
 {
@@ -20,83 +21,91 @@ public:
 	typedef typename Effect::VertexShader::Output VSOut;
 	typedef typename Effect::GeometryShader::Output GSOut;
 public:
-	Pipeline(Graphics& gfx)
+	Pipeline( Graphics& gfx )
 		:
-		gfx(gfx),
-		zb(gfx.ScreenWidth, gfx.ScreenHeight)
+		Pipeline( gfx,std::make_shared<ZBuffer>( gfx.ScreenWidth,gfx.ScreenHeight ) )
 	{}
-	void Draw(IndexedTriangleList<Vertex>& triList)
+	Pipeline( Graphics& gfx,std::shared_ptr<ZBuffer> pZb )
+		:
+		gfx( gfx ),
+		pZb( std::move( pZb ) )
 	{
-		ProcessVertices(triList.vertices, triList.indices);
+		assert( pZb->height == gfx.ScreenHeight && pZb->width == gfx.ScreenWidth );
 	}
-	// need to reset the z-buffer after each frame
+	void Draw( IndexedTriangleList<Vertex>& triList )
+	{
+		ProcessVertices( triList.vertices,triList.indices );
+	}
+	// needed to reset the z-buffer after each frame
 	void BeginFrame()
 	{
-		zb.Clear();
+		pZb->Clear();
 	}
 private:
-	// vertex process function
-	// transforms vertices using vs(vertex shader) and then passes vtx & idx lists to triangle assembler
-	void ProcessVertices(const std::vector<Vertex>& vertices, const std::vector<size_t>& indices)
+	// vertex processing function
+	// transforms vertices using vs and then passes vtx & idx lists to triangle assembler
+	void ProcessVertices( const std::vector<Vertex>& vertices,const std::vector<size_t>& indices )
 	{
 		// create vertex vector for vs output
-		std::vector<VSOut> verticesOut(vertices.size());
+		std::vector<VSOut> verticesOut( vertices.size() );
 
-
-		// transform verteices with vs
-		std::transform(vertices.begin(), vertices.end(), verticesOut.begin(), effect.vs);
+		// transform vertices with vs
+		std::transform( vertices.begin(),vertices.end(),
+						verticesOut.begin(),
+						effect.vs );
 
 		// assemble triangles from stream of indices and vertices
-		AssembleTriangles(verticesOut, indices);
+		AssembleTriangles( verticesOut,indices );
 	}
 	// triangle assembly function
 	// assembles indexed vertex stream into triangles and passes them to post process
 	// culls (does not send) back facing triangles
-	void AssembleTriangles(const std::vector<VSOut>& vertices, const std::vector<size_t>& indices)
+	void AssembleTriangles( const std::vector<VSOut>& vertices,const std::vector<size_t>& indices )
 	{
-		for (size_t i = 0, end = indices.size() / 3; i < end; i++)
+		// assemble triangles in the stream and process
+		for( size_t i = 0,end = indices.size() / 3;
+			 i < end; i++ )
 		{
 			// determine triangle vertices via indexing
 			const auto& v0 = vertices[indices[i * 3]];
 			const auto& v1 = vertices[indices[i * 3 + 1]];
 			const auto& v2 = vertices[indices[i * 3 + 2]];
-
-			// culling backfacing triangles with cross product(%) shenanigans
-			if ((v1.pos - v0.pos) % (v2.pos - v0.pos) * v0.pos <= 0.0f)
+			// cull backfacing triangles with cross product (%) shenanigans
+			if( (v1.pos - v0.pos) % (v2.pos - v0.pos) * v0.pos <= 0.0f )
 			{
 				// process 3 vertices into a triangle
-				ProcessTriangle(v0, v1, v2, i);
+				ProcessTriangle( v0,v1,v2,i );
 			}
 		}
 	}
 	// triangle processing function
-	// passes 3 vertices to gs(geometry shader) to generate triangle
+	// passes 3 vertices to gs to generate triangle
 	// sends generated triangle to post-processing
-	void ProcessTriangle(const VSOut& v0, const VSOut& v1, const VSOut& v2, size_t triangle_index)
+	void ProcessTriangle( const VSOut& v0,const VSOut& v1,const VSOut& v2,size_t triangle_index )
 	{
-		// generate triangle from 3 vertices using gs (geometry shader)
+		// generate triangle from 3 vertices using gs
 		// and send to post-processing
-		PostProcessTriangleVertices(effect.gs(v0, v1, v2, triangle_index));
+		PostProcessTriangleVertices( effect.gs( v0,v1,v2,triangle_index ) );
 	}
 	// vertex post-processing function
-	// perform perspective and viewport transformation
-	void PostProcessTriangleVertices(Triangle<GSOut>& triangle)
+	// perform perspective and viewport transformations
+	void PostProcessTriangleVertices( Triangle<GSOut>& triangle )
 	{
 		// perspective divide and screen transform for all 3 vertices
-		pst.Transform(triangle.v0);
-		pst.Transform(triangle.v1);
-		pst.Transform(triangle.v2);
+		pst.Transform( triangle.v0 );
+		pst.Transform( triangle.v1 );
+		pst.Transform( triangle.v2 );
 
 		// draw the triangle
-		DrawTriangle(triangle);
+		DrawTriangle( triangle );
 	}
-	// === triangle rasterization function ===
+	// === triangle rasterization functions ===
 	//   it0, it1, etc. stand for interpolants
 	//   (values which are interpolated across a triangle in screen space)
 	//
-	// entry point for triangle rasterization
-	// sorts vertices, determines case, splits to flat triangles, dispatches to flat triangle functions
-	void DrawTriangle(const Triangle<GSOut>& triangle)
+	// entry point for tri rasterization
+	// sorts vertices, determines case, splits to flat tris, dispatches to flat tri funcs
+	void DrawTriangle( const Triangle<GSOut>& triangle )
 	{
 		// using pointers so we can swap (for sorting purposes)
 		const GSOut* pv0 = &triangle.v0;
@@ -104,23 +113,23 @@ private:
 		const GSOut* pv2 = &triangle.v2;
 
 		// sorting vertices by y
-		if (pv1->pos.y < pv0->pos.y) std::swap(pv0, pv1);
-		if (pv2->pos.y < pv1->pos.y) std::swap(pv1, pv2);
-		if (pv1->pos.y < pv0->pos.y) std::swap(pv0, pv1);
+		if( pv1->pos.y < pv0->pos.y ) std::swap( pv0,pv1 );
+		if( pv2->pos.y < pv1->pos.y ) std::swap( pv1,pv2 );
+		if( pv1->pos.y < pv0->pos.y ) std::swap( pv0,pv1 );
 
-		if (pv0->pos.y == pv1->pos.y) // natural flat top
+		if( pv0->pos.y == pv1->pos.y ) // natural flat top
 		{
 			// sorting top vertices by x
-			if (pv1->pos.x < pv0->pos.x) std::swap(pv0, pv1);
+			if( pv1->pos.x < pv0->pos.x ) std::swap( pv0,pv1 );
 
-			DrawFlatTopTriangle(*pv0, *pv1, *pv2);
+			DrawFlatTopTriangle( *pv0,*pv1,*pv2 );
 		}
-		else if (pv1->pos.y == pv2->pos.y) // natural flat bottom
+		else if( pv1->pos.y == pv2->pos.y ) // natural flat bottom
 		{
 			// sorting bottom vertices by x
-			if (pv2->pos.x < pv1->pos.x) std::swap(pv1, pv2);
+			if( pv2->pos.x < pv1->pos.x ) std::swap( pv1,pv2 );
 
-			DrawFlatBottomTriangle(*pv0, *pv1, *pv2);
+			DrawFlatBottomTriangle( *pv0,*pv1,*pv2 );
 		}
 		else // general triangle
 		{
@@ -128,24 +137,26 @@ private:
 			const float alphaSplit =
 				(pv1->pos.y - pv0->pos.y) /
 				(pv2->pos.y - pv0->pos.y);
-			const auto vi = interpolate(*pv0, *pv2, alphaSplit);
+			const auto vi = interpolate( *pv0,*pv2,alphaSplit );
 
-			if (pv1->pos.x < vi.pos.x) // major right
+			if( pv1->pos.x < vi.pos.x ) // major right
 			{
-				DrawFlatBottomTriangle(*pv0, *pv1, vi);
-				DrawFlatTopTriangle(*pv1, vi, *pv2);
+				DrawFlatBottomTriangle( *pv0,*pv1,vi );
+				DrawFlatTopTriangle( *pv1,vi,*pv2 );
 			}
 			else // major left
 			{
-				DrawFlatBottomTriangle(*pv0, vi, *pv1);
-				DrawFlatTopTriangle(vi, *pv1, *pv2);
+				DrawFlatBottomTriangle( *pv0,vi,*pv1 );
+				DrawFlatTopTriangle( vi,*pv1,*pv2 );
 			}
 		}
 	}
-	// does flat *TOP* trianlge specific calculations and calls DrawFlatTriangle
-	void DrawFlatTopTriangle(const GSOut& it0, const GSOut& it1, const GSOut& it2)
+	// does flat *TOP* tri-specific calculations and calls DrawFlatTriangle
+	void DrawFlatTopTriangle( const GSOut& it0,
+							  const GSOut& it1,
+							  const GSOut& it2 )
 	{
-		// calculate dVertex / dy
+		// calulcate dVertex / dy
 		// change in interpolant for every 1 change in y
 		const float delta_y = it2.pos.y - it0.pos.y;
 		const auto dit0 = (it2 - it0) / delta_y;
@@ -155,12 +166,14 @@ private:
 		auto itEdge1 = it1;
 
 		// call the flat triangle render routine
-		DrawFlatTriangle(it0, it1, it2, dit0, dit1, itEdge1);
+		DrawFlatTriangle( it0,it1,it2,dit0,dit1,itEdge1 );
 	}
-	// does flat *BOTTOM* trianlge specific calculations and calls DrawFlatTriangle
-	void DrawFlatBottomTriangle(const GSOut& it0, const GSOut& it1, const GSOut& it2)
+	// does flat *BOTTOM* tri-specific calculations and calls DrawFlatTriangle
+	void DrawFlatBottomTriangle( const GSOut& it0,
+								 const GSOut& it1,
+								 const GSOut& it2 )
 	{
-		// calculate dVertex / dy
+		// calulcate dVertex / dy
 		// change in interpolant for every 1 change in y
 		const float delta_y = it2.pos.y - it0.pos.y;
 		const auto dit0 = (it1 - it0) / delta_y;
@@ -170,58 +183,62 @@ private:
 		auto itEdge1 = it0;
 
 		// call the flat triangle render routine
-		DrawFlatTriangle(it0, it1, it2, dit0, dit1, itEdge1);
-
+		DrawFlatTriangle( it0,it1,it2,dit0,dit1,itEdge1 );
 	}
 	// does processing common to both flat top and flat bottom tris
 	// scan over triangle in screen space, interpolate attributes,
 	// depth cull, invoke ps and write pixel to screen
-	void DrawFlatTriangle(const GSOut& it0, const GSOut& it1, const GSOut& it2,
-		const GSOut& dv0, const GSOut& dv1, GSOut itEdge1)
+	void DrawFlatTriangle( const GSOut& it0,
+						   const GSOut& it1,
+						   const GSOut& it2,
+						   const GSOut& dv0,
+						   const GSOut& dv1,
+						   GSOut itEdge1 )
 	{
 		// create edge interpolant for left edge (always v0)
 		auto itEdge0 = it0;
 
 		// calculate start and end scanlines
-		const int yStart = (int)ceilf(it0.pos.y - 0.5f);
-		const int yEnd = (int)ceilf(it2.pos.y - 0.5f); // the scanline AFTER the last line drawn
+		const int yStart = (int)ceil( it0.pos.y - 0.5f );
+		const int yEnd = (int)ceil( it2.pos.y - 0.5f ); // the scanline AFTER the last line drawn
 
 		// do interpolant prestep
-		itEdge0 += dv0 * (float(yStart) + 0.5f - it0.pos.y);
-		itEdge1 += dv1 * (float(yStart) + 0.5f - it0.pos.y);
+		itEdge0 += dv0 * (float( yStart ) + 0.5f - it0.pos.y);
+		itEdge1 += dv1 * (float( yStart ) + 0.5f - it0.pos.y);
 
-		for (int y = yStart; y < yEnd; y++, itEdge0 += dv0, itEdge1 += dv1)
+		for( int y = yStart; y < yEnd; y++,itEdge0 += dv0,itEdge1 += dv1 )
 		{
 			// calculate start and end pixels
-			const int xStart = (int)ceilf(itEdge0.pos.x - 0.5f);
-			const int xEnd = (int)ceilf(itEdge1.pos.x - 0.5f); // the pixel AFTER the last pixel drawn
+			const int xStart = (int)ceil( itEdge0.pos.x - 0.5f );
+			const int xEnd = (int)ceil( itEdge1.pos.x - 0.5f ); // the pixel AFTER the last pixel drawn
 
 			// create scanline interpolant startpoint
 			// (some waste for interpolating x,y,z, but makes life easier not having
-			//  to split them off, and z will be needed in the future anyways ...)
+			//  to split them off, and z will be needed in the future anyways...)
 			auto iLine = itEdge0;
 
 			// calculate delta scanline interpolant / dx
 			const float dx = itEdge1.pos.x - itEdge0.pos.x;
-			const auto diLine = (itEdge1 - itEdge0) / dx;
+			const auto diLine = (itEdge1 - iLine) / dx;
 
 			// prestep scanline interpolant
-			iLine += diLine * (float(xStart) + 0.5f - itEdge0.pos.x);
+			iLine += diLine * (float( xStart ) + 0.5f - itEdge0.pos.x);
 
-			for (int x = xStart; x < xEnd; x++, iLine += diLine)
+			for( int x = xStart; x < xEnd; x++,iLine += diLine )
 			{
-				// recover interpolated z from iterpolated 1/z
+				// recover interpolated z from interpolated 1/z
 				const float z = 1.0f / iLine.pos.z;
 				// do z rejection / update of z buffer
 				// skip shading step if z rejected (early z)
-				if (zb.TestAndSet(x, y, z))
+				if( pZb->TestAndSet( x,y,z ) )
 				{
 					// recover interpolated attributes
-					// (wasted effort in multiplying pos(x, y, z), but no big deal)
+					// (wasted effort in multiplying pos (x,y,z) here, but
+					//  not a huge deal, not worth the code complication to fix)
 					const auto attr = iLine * z;
 					// invoke pixel shader with interpolated vertex attributes
 					// and use result to set the pixel color on the screen
-					gfx.PutPixel(x, y, effect.ps(attr));
+					gfx.PutPixel( x,y,effect.ps( attr ) );
 				}
 			}
 		}
@@ -231,7 +248,5 @@ public:
 private:
 	Graphics& gfx;
 	PubeScreenTransformer pst;
-	ZBuffer zb;
-	Mat3 rotation;
-	Vec3 translation;
+	std::shared_ptr<ZBuffer> pZb;
 };
